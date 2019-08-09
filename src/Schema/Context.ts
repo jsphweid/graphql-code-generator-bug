a03d7ca659debca013a3dc900f2c27a54a306e8d
Joseph Weidinger <josephweidinger@carfax.com>
2019-08-08 19:34:33 -0500

    even better

--- src/Schema/Context.ts
+++ src/Schema/Context.ts
@@ -1,13 +1,11 @@
-import Moment from "moment";
-
 // NODE: simply commenting this out gets codegen to work again
 import * as Archive from "../../../raw-archive/src";
 
 export interface Context {
-  now: Moment.Moment;
+  now: Date;
   archive: Archive.Archive;
 }
 
 export const context = async (): Promise<Context> => {
-  return { now: Moment(), archive: Archive.makeArchive({}) };
+  return { now: new Date(), archive: Archive.makeArchive({}) };
 };
